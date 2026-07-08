508ad889223aa72bc74ebabab253c4d122bafd1b
CZHczhABCZ <3772522570@qq.com>
2026-07-08 18:52:38 +0800

    feat: add ShowCameo.CustomPCX support for custom PCX cameo icons

--- src/Ext/TechnoType/Body.h
+++ src/Ext/TechnoType/Body.h
@@ -533,6 +533,7 @@ public:
 		CustomPalette CameoPalette;
 		CustomPalette ShowCameo_CustomPalette;
 		TheaterSpecificSHP ShowCameo_CustomShape;
+		PhobosPCXFile ShowCameo_CustomPCX;
 
 		ExtData(TechnoTypeClass* OwnerObject) : Extension<TechnoTypeClass>(OwnerObject)
 			, HealthBar_Hide { false }
@@ -1020,6 +1021,7 @@ public:
 			, CameoPalette {}
 			, ShowCameo_CustomPalette {}
 			, ShowCameo_CustomShape {}
+			, ShowCameo_CustomPCX {}
 		{ }
 
 		virtual ~ExtData() = default;
